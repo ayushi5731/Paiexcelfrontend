7f96315fa90f3eed701ff7c433a6f09e0b3b04c7
ayushi arora <ayushiarora5731@gmail.com>
2025-06-16 23:18:20 +0530

    responsive code restored

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,44 +1,4 @@
-// import React from "react";
-// import Mission from "../components/Mission";
-// import Batches from "../components/Batches";
-// import ProgramDetails from "../components/ProgramDetails";
-// import Instructor from "../components/Instructor";
-// import StudentGains from "../components/StudentGains";
-// // import { useTheme } from "../contexts/ThemeContext"
-// // import LanguageToggle from "./LanguageToggle"
-// // import ThemeSwitcher from "./ThemeSwitcher"
-// import Hero from "../components/Hero";
-
-// function Homepage() {
-
-//   return (
-//     <main>
-//       <Hero />      
-//    <ProgramDetails />
-//       <Instructor />
-//       <Mission />
-//       <Batches />
-//       {/* <PdfEmbed /> */}
-    
-//       <StudentGains />
-//     </main>
-//   );
-// }
-
-// export default Homepage;
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ayushii
 import React from "react";
 import Mission from "../components/Mission";
 import Batches from "../components/Batches";
